6f81a71b37010ffe1d73b4c4f8d026771c244fd0
helgakern <helgakern@gmail.com>
2020-06-03 13:42:34 -0700

    created hook

--- src/NewQuestionForm/index.js
+++ src/NewQuestionForm/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 function NewQuestionForm(props) {
+  const { newQuestionData, updateQuestionData } = props;
+
   function handleSubmit(event) {
     event.preventDefault();
+    // const { currentTarget } = event;
+    // const formData = new FormData(currentTarget);
+    // props.createQuestion({
+    //   title: formData.get("title"),
+    //   body: formData.get("body"),
+    // });
+    props.createQuestion()
+  }
+
+  function handleQuestionInput(event) {
     const { currentTarget } = event;
-    const formData = new FormData(currentTarget);
-    props.createQuestion({
-      title: formData.get("title"),
-      body: formData.get("body"),
-    });
+    const { value, name } = currentTarget
+    updateQuestionData({[name]: value})
   }
 
   return (
@@ -19,12 +28,24 @@ function NewQuestionForm(props) {
       <div className="field">
         <label htmlFor="title">Title</label>
         <br />
-        <input name="title" id="title"></input>
+        <input 
+          name="title"
+          id="title"
+          value={newQuestionData.title}
+          onChange={handleQuestionInput}
+        >
+        </input>
       </div>
       <div className="field">
         <label htmlFor="body">Body</label>
         <br />
-        <textarea name="body" id="body"></textarea>
+        <textarea 
+          name="body"
+          id="body"
+          value={newQuestionData.body}
+          onChange={handleQuestionInput}
+          >
+        </textarea>
       </div>
       <input
         className="ui right floated large orange button"
--- src/NewQuestionPage/index.js
+++ src/NewQuestionPage/index.js
@@ -5,20 +5,40 @@ import { Question } from "../requests";
 class NewQuestionPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newQuestionData: {
+        title: '',
+        body: ''
+      }
+    }
     this.createQuestion = this.createQuestion.bind(this);
+    this.updateQuestionData = this.updateQuestionData.bind(this);
   }
 
-  createQuestion(params) {
-    Question.create(params).then((question) => {
+  createQuestion() {
+    Question.create(this.state.newQuestionData).then((question) => {
       this.props.history.push(`/questions/${question.id}`);
     });
+  }
+
+  updateQuestionData(props) {
+    this.setState((state) => {
+      console.log(props)
+      console.log(state)
+      return {
+        newQuestionData: {
+          ...state.newQuestionData,
+          ...props
+        }
+      }
+    })
   }
 
   render() {
     return (
       <main>
         <h1 className="ui horizontal divider header">New Question page</h1>
-        <NewQuestionForm createQuestion={this.createQuestion} />
+        <NewQuestionForm createQuestion={this.createQuestion} newQuestionData={this.state.newQuestionData} updateQuestionData={this.updateQuestionData}/>
       </main>
     );
   }
